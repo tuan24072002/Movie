6e18a04ca7b22feacbfdc6b8c558bb38b9027497
tuan24072002 <44106906+tuan24072002@users.noreply.github.com>
2024-10-09 15:39:42 +0700

    finish homepage

--- backend/index.js
+++ backend/index.js
@@ -21,12 +21,21 @@ app.get("/", (req, res) => {
     res.send(req.headers?.authorization?.split(" ")[1] || "Hello world");
 });
 const __dirname = path.resolve();
+
 app.use("/backend/uploads", express.static(`${__dirname}/backend/uploads`))
 app.use("/api/users", userRouter)
 app.use("/api/movies", movieRouter)
 app.use("/api/categories", categoryRouter)
 app.use("/api/uploads", uploadRouter)
 app.use(errorHandler);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/dist')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+    });
+}
+
 connectDB();
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
